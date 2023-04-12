d9fc07f49d858c4cbefc339e3aecc86164680646
darkstellar16 <itsmeshubham2799@gmail.com>
2023-04-12 16:33:23 +0530

    Added further changes and added delete button

--- controller/home/index.js
+++ controller/home/index.js
@@ -1,19 +1,37 @@
 const data = require("../../models/dataModel.js");
 
 
-const formData = (req, res) => {
-    res.render('data');
+const formData = async (req, res) => {
+    try {
+        const dataInfo = await data.find();
+        console.log(dataInfo);
+        res.render('data', { info: dataInfo })
+    } catch (error) {
+        console.log(error);
+    }
 }
-
 
 
 const taskData = (req, res) => {
 
     const dataInfo = new data({
-        data: req.body.name
+        data: req.body.data
     })
     dataInfo.save();
+    res.redirect('/');
 }
 
+const deleteData = async (req, res) => {
 
-module.exports = { fd:formData,  td: taskData };1   
+    const id = req.params.id;
+    console.log(id);
+
+    await data.deleteOne({ _id: id }).then(() => {
+        res.redirect('/');
+        console.log("deleted");
+    }).catch((error) => {
+        console.log(error);
+    })
+}
+
+module.exports = { fd: formData, td: taskData, dd: deleteData };
--- routes/home/index.js
+++ routes/home/index.js
@@ -2,25 +2,19 @@ const express = require("express");
 
 const route = express.Router();
 
-const data = require("../../models/dataModel.js");
+// const data = require("../../models/dataModel.js");
 
-const taskData = require("../../controller/home/index.js");
+const taskData = require("../../controller/home/index.js"); //collected task data
 
-const formData = require("../../controller/home/index.js");
+const formData = require("../../controller/home/index.js"); //collected form data
 
-route.get('/', formData.fd);  //To render the form on the home page..
+const deleteData = require('../../controller/home/index.js'); //collected delete data
 
-route.post('/', taskData.td); //To pass the form data to the database through post route...
+route.get('/', formData.fd);  //To render the form on the home page.....
 
+route.post('/', taskData.td); //To pass the form data to the database through post route......
 
-
-
-
-
-
-
-
-
+route.get('/:id', deleteData.dd)  //TO delete the task data from the database through get route......
 
 
 module.exports = route;
